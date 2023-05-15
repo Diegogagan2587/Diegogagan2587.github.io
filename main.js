ee3d09d1980f687aeecf73109190d505d5ba66ae
Zven94 <125831572+Zven94@users.noreply.github.com>
2023-05-15 14:38:51 -0300

    Added exit button for movile menu

--- main.js
+++ main.js
@@ -9,10 +9,21 @@ function displayMobileMenu(){
     }
 }
 
+function exitMobileMenu(){
+    console.log('runing exitMobileMenu');
+    const navMobileList = document.getElementById('nav-desktop-list');
+    var menuDisplay = window.getComputedStyle(navMobileList).display;
+    if(menuDisplay === 'block') {
+        navMobileList.style.display = 'none';
+    }
+}
+
 //heper functiosn upwards;
 
 //atach event listener and add the fuction
+const exit = document.getElementById('exit');
 const hamburgerBtn = document.getElementById('hamburgerBtn');//sintax is correct;
 console.log('navIconMenu ===',hamburgerBtn);
 hamburgerBtn.addEventListener('click', displayMobileMenu);
+exit.addEventListener('click', exitMobileMenu);
 
