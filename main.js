c3e619812be3e13f66a0f3282b4d9e987a6f5b62
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-05-16 12:14:06 -0600

    fix linterns erros for file main.js

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-function displayMobileMenu(){
-    document.getElementById('nav-list-top').className = "nav-list-top-dropMobile";
+function displayMobileMenu() {
+  document.getElementById('nav-list-top').className = 'nav-list-top-dropMobile';
 }
 
-function exitMobileMenu(){
-    document.getElementById('nav-list-top').className = "nav-list-top";
+function exitMobileMenu() {
+  document.getElementById('nav-list-top').className = 'nav-list-top';
 }
 
-//heper functiosn upwards;
+// heper functiosn upwards;
 const exit = document.getElementById('exit');
 const navListAnchors = document.getElementById('nav-list-top').querySelectorAll('a');
 const hamburgerBtn = document.getElementById('hamburgerBtn');
 
-//add event listeners;
+// add event listeners;
 hamburgerBtn.addEventListener('click', displayMobileMenu);
 exit.addEventListener('click', exitMobileMenu);
-for(let i =0; i<navListAnchors.length; i++){
-    navListAnchors[i].addEventListener('click', exitMobileMenu);
+for (let i = 0; i < navListAnchors.length; i += 1) {
+  navListAnchors[i].addEventListener('click', exitMobileMenu);
 }
-
