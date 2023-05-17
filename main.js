300ca87d2e8d03e7b451e27ba21e1b9cac613501
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-05-17 15:28:38 -0600

    fix technologies tags appending when a popup window was displayed, no more extra tags now

--- main.js
+++ main.js
@@ -11,7 +11,10 @@ function openPopUp() {
 }
 
 function exitPopUp() {
+  console.log('we are exit from popup')
   document.getElementById('portfolio-pupUp-background').className = 'portfolio-pupUp-backgroundClose';
+  document.getElementById('portfolio-pupUp-background').querySelector('.technology-tags').innerHTML = null;
+  console.log( document.getElementById('portfolio-pupUp-background').querySelector('.technology-tags'));
 }
 
 // heper functiosn upwards;
@@ -42,7 +45,7 @@ function JobCardData (name, description, img, technologies, liveVersion, source)
 const jobCards = [
    new JobCardData (
     'nickson',
-    '1----loremimpus asljdfasdfasdfasdfasdf',
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     './img/desktop/01Snapshoot Portfolio.svg',
     ['javat1', 'htmlt1', 'csst'],
     'https://diegogagan2587.github.io/Diego-Vidal',
@@ -133,8 +136,6 @@ function setPopUpData(e){
   //tech
   const tagsData = jobCards[dataIndex]['technologies'];
   const tagsContainer = popUpContainer.querySelector('.technology-tags');
-  //const tagsElements = popUpContainer.querySelector('.technology-tags').querySelectorAll('li');
-  //console.log('tags data=', tagsData)
   
   for (let i = 0; i<jobCards[dataIndex]['technologies'].length; i += 1 ) { 
     let newListElement = document.createElement('li');
