4ba065ad19ba4c7cc3cd47cce4a23c7bfe8f25be
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 09:49:11 -0600

    Implemente Intro Header bakcground

--- src/sections/IntroductionSection.jsx
+++ src/sections/IntroductionSection.jsx
@@ -1,8 +1,20 @@
 import SocialMediaBar from '../components/SocialMediaBar';
+import background from '../assets/img/header-shapes-mobile@2x.svg';
 
 const IntroductionSection = () => {
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize:   '100%',
+  };
+
   return (
-    <section id="introduction-section" className="p-6">
+    <section
+      id="introduction-section"
+      className="p-6 border-t-[36px] w-full bg-white h-screen flex flex-col justify-center items-center
+      rounded-bl-[64px] border-2"
+      style={backgroundStyle}
+    >
       <div className="border-2">
         <h1 className="text-2xl">I'm Diego, Glad to see you !</h1>
         <p>
