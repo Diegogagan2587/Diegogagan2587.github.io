6720f7117b7a1d5e15afa33f98820441673985a3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 07:47:47 -0600

    Import Project Data and reder ProjectCards

--- src/sections/PortfolioSection.jsx
+++ src/sections/PortfolioSection.jsx
@@ -1,7 +1,13 @@
+import projects from "../data/projects";
+import ProjectCard from "../components/ProjectCard";
 const PortfolioSection = () => {
   return (
     <section id="portfolio" className="portfolio">
       <h1>Projects willl go here</h1>
+      {/* for each project we will render a component*/}
+      {projects.map((project) => {
+        return <ProjectCard key={project.name} project={project} />;
+      })}
     </section>
   );
 };
