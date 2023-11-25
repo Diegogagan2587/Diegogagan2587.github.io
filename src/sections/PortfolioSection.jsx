d1b4cedd040fe48f0e46173e36a05ffddd11765b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 14:04:39 -0600

    Update position ans set gaps for JobCards within Portfolio section

--- src/sections/PortfolioSection.jsx
+++ src/sections/PortfolioSection.jsx
@@ -2,8 +2,7 @@ import projects from "../data/projects";
 import ProjectCard from "../components/ProjectCard";
 const PortfolioSection = () => {
   return (
-    <section id="portfolio" className="portfolio px-6 flex flex-col gap-5">
-      <h1>Projects willl go here</h1>
+    <section id="portfolio" className="portfolio px-6 py-28 flex flex-col gap-24">
       {/* for each project we will render a component*/}
       {projects.map((project) => {
         return <ProjectCard key={project.name} project={project} />;
