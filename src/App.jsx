51c1a61efa3fb9f9c9b8abc329e6fc98891f0654
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-24 22:24:30 -0600

    Import navigationbar form within App and implement basic mobile menu button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavigationBar from './components/NavigationBar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <header>
+        <NavigationBar />
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 
 const NavigationBar = () => {
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log('Menu clicked', isMenuOpen)
+  }
   return (
     <nav className="flex border-2">
       <span>DVLoper</span>
       <div>
-
+      <span className='sm:hidden' onClick={handleMenuClick}>
+          {isMenuOpen ? 'X' : 'Menu'}
+        </span>
       </div>
       <ul className="flex gap-5">
         <li>
