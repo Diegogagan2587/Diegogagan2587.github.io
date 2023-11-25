b81a52d0c3287abb93e42c0e76e4bd5f48b6b2f8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-24 20:54:36 -0600

    Fix build output

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build.outDir: './docs',
+  build: {
+    outDir: './docs',
+  },
   base: '/Diego-Vidal/',
   plugins: [react()],
 })
