b234df8d8bbaf792ec4927f71211e9a0d3cc19c7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 15:00:07 -0600

    Add AboutMyself container within about section

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -1,28 +1,29 @@
+import Btn from '../components/Btn';
+import SocialMediaBar from '../components/SocialMediaBar';
+
 const AboutSection = () => {
-    return (
-        <section id="about" className="border-2">
-            <h2>About Me</h2>
-            <p>
-            I am a Full Stack Developer with a passion for learning and creating
-            new things. I have a background in the medical field and I am
-            currently working as a Medical Assistant. I have always been
-            interested in technology and I am excited to be a part of the
-            ever-changing tech industry.
-            </p>
-            <p>
-            I am a graduate of the University of California, Irvine Full Stack
-            Web Development Bootcamp. I have experience with HTML, CSS,
-            JavaScript, Node.js, Express.js, React.js, MongoDB, MySQL, and
-            Handlebars.js. I am a creative problem solver and I enjoy working
-            with others to create something new.
-            </p>
-            <p>
-            When I am not coding, I enjoy spending time with my family and
-            friends. I also enjoy watching movies, reading, and playing video
-            games.
-            </p>
-        </section>
-    )
+  return (
+    <section
+      id="about"
+      className="flex flex-col items-center justify-center
+      h-screen border-2 bg-white rounded-tr-[50px] px-6"
+    >
+      <div id="mySelf" className="flex flex-col gap-3 border-2">
+        <h2 className="font-bold text-4xl text-[#172B4D]">About Myself</h2>
+        <p className="text-[#344563]">
+          Hello I’m a software developer! I can help you build a product ,
+          feature or website. Look through some of my work and experience! If
+          you like what you see and have a project you need coded, don’t
+          hesitate to contact me.
+        </p>
+        <SocialMediaBar />
+        <div>
+          <Btn text="Get my resume" />
+        </div>
+      </div>
+      <div id="skill-list"></div>
+    </section>
+  );
 };
 
 export default AboutSection;
