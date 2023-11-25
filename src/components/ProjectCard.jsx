01720649ebe4e4d7258214e7b9586ca0e4f1b189
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 10:36:07 -0600

    Set Basic laout, position and sizes for elements

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,14 +1,17 @@
 const ProjectCard = ({ project }) => {
-    return (
-        <div className="card">
-        <div className="card-body">
-            <h5 className="card-title">{project.name}</h5>
-            <p className="card-text">{project.description}</p>
-            <a href={project.liveVersion} className="btn btn-primary">
-            Go to project
-            </a>
-        </div>
-        </div>
-    );
+  return (
+    <div className="card bg-white border-2 rounded-xl p-4 flex flex-col gap-3">
+      <div className="container h-10 w-full bg-green-100 min-w-[295px] min-h-[220px]">
+        {/* images will go here */}
+      </div>
+      <div className="card-body min-w-[295px] min-h-[288px] border-2">
+        <h5 className="card-title">{project.name}</h5>
+        <p className="card-text">{project.description}</p>
+        <a href={project.liveVersion} className="btn btn-primary">
+          Go to project
+        </a>
+      </div>
+    </div>
+  );
 };
 export default ProjectCard;
