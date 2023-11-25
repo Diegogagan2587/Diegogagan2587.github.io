acb22960fffb6eaf402fe9f3aab44bd26abae5a7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 15:12:02 -0600

    Create file for storing skills

---
+++ src/data/skills.js
@@ -0,0 +1,27 @@
+const skills = [
+    {
+        name: 'Languages',
+        items: [
+            'JavaScript',
+            'HTML',
+            'CSS',
+        ],
+    },
+    {
+        name: 'Libraries/Frameworks',
+        items: [
+            'React',
+            'Redux',
+        ],
+    }
+    {
+        name: 'Skills',
+        items: [
+            'Pair Programming',
+            'Agile',
+            'Test Driven Development',
+        ],
+    },
+];
+
+export default skills;
