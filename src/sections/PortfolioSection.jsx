ef1c3f30293cf5421ace9934f1ee521e033a2aff
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 12:29:06 -0600

    Add padding and gap between each project card

--- src/sections/PortfolioSection.jsx
+++ src/sections/PortfolioSection.jsx
@@ -2,7 +2,7 @@ import projects from "../data/projects";
 import ProjectCard from "../components/ProjectCard";
 const PortfolioSection = () => {
   return (
-    <section id="portfolio" className="portfolio">
+    <section id="portfolio" className="portfolio px-6 flex flex-col gap-5">
       <h1>Projects willl go here</h1>
       {/* for each project we will render a component*/}
       {projects.map((project) => {
