3d27dae2f42056707e17c40d97e91897b7768634
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 17:52:53 -0600

    Set positions and basic styling, refactor to accept icons from src/data/skills.js

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,28 @@
 import skills from '../data/skills';
-import DropDownIcon from '../assets/icons/Disabledbutton-down-arrow.png'
+import DropDownIcon from '../assets/icons/Disabledbutton-down-arrow.png';
 const Skills = () => {
   return (
     <div id="skills" className="border-2 w-full px-1">
       {skills.map((skill) => {
         return (
-          <div key={skill.name} 
-          className='px-2'
-          >
-            <div className='flex justify-between items-center py-2 border-2'>
-              <h3 className='font-medium text-xl'>{skill.name}</h3>
+          <div key={skill.name} className="px-2">
+            <div className="flex justify-between items-center py-2 border-2">
+              <h3 className="font-medium text-xl">{skill.name}</h3>
               <img src={DropDownIcon} alt="DropDown" />
             </div>
-            <ul>
+            <ul className='flex flex-col gap-3'>
               {skill.list.map((item) => {
-                return <li key={item}>{item}</li>;
+                return (
+                  <li key={item.name} className="flex items-center p-2 bg-slate-100 rounded-lg">
+                    <div className="w-12 h-12 bg-slate-500 rounded-full">
+                      <img src={item.icon} alt="Language-Icon"
+                      className='w-full h-full object-cover rounded-full'
+                      />
+                    </div>
+
+                    <p>{item.name}</p>
+                  </li>
+                );
               })}
             </ul>
           </div>
