cfbb26c57454116ae8e6a5f40371167eae928a7a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 13:07:46 -0600

    Implement Styling for Btn.jsx component

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -2,7 +2,14 @@ import PropTypes from 'prop-types';
 
 const Btn = ({ text, onClick, type }) => {
   return (
-    <button type={type} onClick={onClick} className="border-2 bg-red-100">
+    <button 
+    type={type} 
+    onClick={onClick} 
+    className="border-2 border-[#7F8CFF]  min-w-20 h-12 rounded-lg p-3
+    text-[#6070FF] font-medium
+    flex items-center justify-center
+    hover:bg-[#6070FF] hover:text-white
+    active:bg-[#2230D2] active:text-white">
       {text}
     </button>
   );
