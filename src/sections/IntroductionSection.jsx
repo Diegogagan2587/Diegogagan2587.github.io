0fb1756803d8c63ad2bb75107633338880a1bec6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 10:09:24 -0600

    header-mobile: Set colors, fontsize and possition all ellements to match design

--- src/sections/IntroductionSection.jsx
+++ src/sections/IntroductionSection.jsx
@@ -15,15 +15,15 @@ const IntroductionSection = () => {
       rounded-bl-[64px] border-2"
       style={backgroundStyle}
     >
-      <div className="border-2">
-        <h1 className="text-2xl">I'm Diego, Glad to see you !</h1>
-        <p>
-          I’m a software developer! I can help you build a product , feature or
+      <div className="border-2 flex flex-col gap-5">
+        <h1 className="text-4xl font-bold leading-[52px] text-[#172B4D]">I'm Diego.<br/> Glad to see you !</h1>
+        <p className='text-base leading-6 text-[#344563]'>
+          I’m a software developer! I can help you build a product, feature, or
           website Look through some of my work and experience! If you like what
           you see and have a project you need coded, don’t hesitate to contact
           me.
-        </p>
-        <span>LET'S CONNECT</span>
+        </p >
+        <span className='text-base font-medium text-[#7F8CFF]'>LET'S CONNECT</span>
         <SocialMediaBar />
       </div>
     </section>
