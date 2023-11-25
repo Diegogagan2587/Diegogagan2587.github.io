3ca215ff25caa95997d7903965e35a61fe3e6ae4
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 17:52:00 -0600

    Refactor src/data/skills.js to store skills and path to icons

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,25 +1,27 @@
+import JavaScriptIcon from '../assets/icons/skills/icons8-javascript-96.png';
+
 const skills = [
     {
         name: 'Languages',
         list: [
-            'JavaScript',
-            'HTML',
-            'CSS',
+            {name:'JavaScript', icon:JavaScriptIcon },
+            {name:'HTML', icon:JavaScriptIcon },
+            {name:'CSS', icon:JavaScriptIcon },
         ],
     },
     {
         name: 'Libraries/Frameworks',
         list: [
-            'React',
-            'Redux',
+            {name:'React', icon:JavaScriptIcon },
+            {name:'Redux', icon:JavaScriptIcon },
         ],
     },
     {
         name: 'Skills',
         list: [
-            'Pair Programming',
-            'Agile',
-            'Test Driven Development',
+            {name:'Pair Programming', icon:JavaScriptIcon },
+            {name:'Agile', icon:JavaScriptIcon },
+            {name:'Test Driven Development', icon:JavaScriptIcon },
         ],
     },
 ];
