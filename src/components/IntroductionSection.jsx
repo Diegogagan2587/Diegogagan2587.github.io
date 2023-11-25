a031d40edcd50514c1e792a96d2380bc36848158
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 06:47:34 -0600

    Introduction Section: Set basi layout

--- src/components/IntroductionSection.jsx
+++ src/components/IntroductionSection.jsx
@@ -1,11 +1,21 @@
+import SocialMediaBar from './SocialMediaBar';
+
 const IntroductionSection = () => {
-    return (
-        <section className="introduction-section">
-        <h1 className="introduction-section__title">Hello, I'm <span className="introduction-section__title--highlight">Luis</span></h1>
-        <p className="introduction-section__description">I'm a web developer based in Mexico City, Mexico. I have a passion for web development and love to create for web and mobile devices.</p>
-        <button className="introduction-section__button">View my work</button>
-        </section>
-    );
+  return (
+    <section id="introduction-section" className="p-6">
+      <div className="border-2">
+        <h1 className="text-2xl">I'm Diego, Glad to see you !</h1>
+        <p>
+          I’m a software developer! I can help you build a product , feature or
+          website Look through some of my work and experience! If you like what
+          you see and have a project you need coded, don’t hesitate to contact
+          me.
+        </p>
+        <span>LET'S CONNECT</span>
+        <SocialMediaBar />
+      </div>
+    </section>
+  );
 };
 
 export default IntroductionSection;
