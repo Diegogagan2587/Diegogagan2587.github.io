dd09801b911061e982b097fd5bd84900322e3eee
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 07:06:27 -0600

    Create ContactForm.jsx placeholder

---
+++ src/sections/ContactForm.jsx
@@ -0,0 +1,23 @@
+const ContactForm = () => {
+    return (
+        <div id="contact-form">
+            <h2>Contact Me</h2>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message:</label>
+                    <textarea id="message" name="message" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+};
+export default ContactForm;
