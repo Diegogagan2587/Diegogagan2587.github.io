273b1d6a0bddb92cb9a221757b7c5b2ec92667b5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 07:05:59 -0600

    Create About Section placeholder

---
+++ src/sections/AboutSection.jsx
@@ -0,0 +1,28 @@
+const AboutSection = () => {
+    return (
+        <section id="about" className="border-2">
+            <h2>About Me</h2>
+            <p>
+            I am a Full Stack Developer with a passion for learning and creating
+            new things. I have a background in the medical field and I am
+            currently working as a Medical Assistant. I have always been
+            interested in technology and I am excited to be a part of the
+            ever-changing tech industry.
+            </p>
+            <p>
+            I am a graduate of the University of California, Irvine Full Stack
+            Web Development Bootcamp. I have experience with HTML, CSS,
+            JavaScript, Node.js, Express.js, React.js, MongoDB, MySQL, and
+            Handlebars.js. I am a creative problem solver and I enjoy working
+            with others to create something new.
+            </p>
+            <p>
+            When I am not coding, I enjoy spending time with my family and
+            friends. I also enjoy watching movies, reading, and playing video
+            games.
+            </p>
+        </section>
+    )
+};
+
+export default AboutSection;
