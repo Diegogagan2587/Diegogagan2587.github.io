6434eada0f4f4e90b8f5c669199549dfa07fa1c0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 00:05:33 -0600

    Create Component: SocialMediaBar.jsx

---
+++ src/components/SocialMediaBar.jsx
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faXTwitter,
+  faLinkedinIn,
+  faGithub,
+  faAngellist,
+  faMedium,
+} from '@fortawesome/free-brands-svg-icons';
+
+const SocialMediaBar = () => {
+  return (
+    <div id="social-media">
+      <h2>Social Media</h2>
+      <ul className="flex">
+        <li>
+          <a href="">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FontAwesomeIcon icon={faXTwitter} />
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+          <FontAwesomeIcon icon={faAngellist} />
+          </a>
+        </li>
+        <li>
+          <a href="">
+          <FontAwesomeIcon icon={faMedium} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default SocialMediaBar;
