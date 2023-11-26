8da6065dbd295d24fb19b0416914b71996aaa4c9
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 21:36:00 -0600

    Update NavigationBar: background and menu to hamburguer icon

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,25 +1,39 @@
 import React, { useState } from 'react';
+import Hamburguer from '../assets/icons/Icon-Menunav-button.png';
+import CloseIcon from '../assets/icons/close-popup-button.png';
 
 const NavigationBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
-    console.log('Menu clicked', isMenuOpen)
-  }
+  };
   return (
-    <nav className="fixed flex justify-between px-5
+    <nav
+      className="fixed flex justify-between items-center px-5
     w-full
-    border-2 bg-gray-100 h-10">
+    border-2 bg-white h-10"
+    >
       <span>DVLoper</span>
       <div>
-      <span className='sm:hidden' onClick={handleMenuClick}>
-          {isMenuOpen ? 'X' : 'Menu'}
+        <span className="sm:hidden" onClick={handleMenuClick}>
+          {(isMenuOpen && <img src={CloseIcon} alt="Close Icon" 
+          className='relative h-full p-2.5'
+          />) || (
+            <img src={Hamburguer} alt="Hamburguer Icon" 
+            className='h-7'/>
+          )}
         </span>
       </div>
-      <ul className={`
-      ${isMenuOpen ? 'block absolute w-screen sm:w-auto h-screen sm:h-auto left-0 sm:left-auto top-10 sm:top-auto' : 'hidden'} 
+      <ul
+        className={`
+      ${
+        isMenuOpen
+          ? 'block absolute w-screen sm:w-auto h-screen sm:h-auto left-0 sm:left-auto top-10 sm:top-auto'
+          : 'hidden'
+      } 
       sm:relative sm:flex p-5 sm:p-0
-       gap-5  bg-green-100`}>
+       gap-5  bg-green-100`}
+      >
         <li>
           <a href="#portfolio">Portfolio</a>
         </li>
