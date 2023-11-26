c0a5d6c5cadc5bc8e8cdfe09a27c60e900feec8f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-25 19:38:06 -0600

    fix backgroun dolor for Btn.jsx component

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -6,7 +6,7 @@ const Btn = ({ text, onClick, type }) => {
     type={type} 
     onClick={onClick} 
     className="border-2 border-[#7F8CFF]  min-w-20 h-12 rounded-lg p-3
-    text-[#6070FF] font-medium
+    text-[#6070FF] bg-white font-medium
     flex items-center justify-center
     hover:bg-[#6070FF] hover:text-white
     active:bg-[#2230D2] active:text-white">
