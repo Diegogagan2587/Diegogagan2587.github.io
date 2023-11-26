1a0ca8d1b8f4c7b015f5f825961ff6bba35e17c8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-26 13:49:05 -0600

    removes un-necesary borders

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -9,7 +9,7 @@ const AboutSection = () => {
       className="flex flex-col sm:flex-row items-center justify-center py-28
       min-h-screen border-2 bg-white rounded-tr-[50px] px-6 gap-6"
     >
-      <div id="mySelf" className="border-2 flex flex-col gap-3
+      <div id="mySelf" className="flex flex-col gap-3
       sm:max-w-[447px] md:w-2/5">
         <h2 className="font-bold text-4xl text-[#172B4D]">About Myself</h2>
         <p className="text-[#344563]">
@@ -24,7 +24,7 @@ const AboutSection = () => {
         </div>
       </div>
 
-      <div id="skill-list" className='w-full border-2
+      <div id="skill-list" className='w-full 
       sm:max-w-[684px] md:w-3/5'>
         <Skills />
       </div>
