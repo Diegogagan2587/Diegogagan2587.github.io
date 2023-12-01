9b92e6009ca1e9a9bf79f6dbee006afed74c6ad3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-30 19:04:21 -0600

    Fix Social Icons resize effect not being working properly due to hidden overflow

--- src/sections/IntroductionSection.jsx
+++ src/sections/IntroductionSection.jsx
@@ -18,28 +18,30 @@ const IntroductionSection = () => {
       "
       //style={backgroundStyle}
     >
-      <div className="flex flex-col gap-5 max-w-[920px] overflow-hidden">
-        <h1 className={`text-4xl font-bold leading-[52px] text-[#172B4D]
-              ${animate ? 'transform translate-x-0 opacity-100' : 'transform translate-x-full opacity-0'}
-              transition-transform ease-in-out duration-500 
-        `}>
-          I&apos;m Diego Vidal
-          <br />
-          Full-stack Web Developer.
-        </h1>
-        <p className={`text-base leading-6 text-[#344563]
-         ${animate ? 'transform translate-x-0 opacity-100' : 'transform translate-x-full opacity-0'}
-         transition-transform ease-in-out duration-500 delay-100
-        `}>
-          I&apos;m a seasoned Full-Stack Web Developer, adept at crafting
-          functional Single Page Applications, robust Server Side Rendering
-          Websites, and powerful APIs. Specializing in JavaScript, React, Redux,
-          Tailwind-CSS, Ruby on Rails, PostgreSQL, and more, I bring over 2,500
-          hours of hands-on experience in building, testing, and debugging
-          software.
-          <br /> Check out my work below or reach out to me in the Contact
-          Section.
-        </p>
+      <div className="flex flex-col gap-5 max-w-[920px]">
+        <div className='flex flex-col gap-5 overflow-hidden'>
+          <h1 className={`text-4xl font-bold leading-[52px] text-[#172B4D]
+                ${animate ? 'transform translate-x-0 opacity-100' : 'transform translate-x-full opacity-0'}
+                transition-transform ease-in-out duration-500 
+          `}>
+            I&apos;m Diego Vidal
+            <br />
+            Full-stack Web Developer.
+          </h1>
+          <p className={`text-base leading-6 text-[#344563]
+          ${animate ? 'transform translate-x-0 opacity-100' : 'transform translate-x-full opacity-0'}
+          transition-transform ease-in-out duration-500 delay-100
+          `}>
+            I&apos;m a seasoned Full-Stack Web Developer, adept at crafting
+            functional Single Page Applications, robust Server Side Rendering
+            Websites, and powerful APIs. Specializing in JavaScript, React, Redux,
+            Tailwind-CSS, Ruby on Rails, PostgreSQL, and more, I bring over 2,500
+            hours of hands-on experience in building, testing, and debugging
+            software.
+            <br /> Check out my work below or reach out to me in the Contact
+            Section.
+          </p>
+        </div>
         <SocialMediaBar />
       </div>
     </section>
