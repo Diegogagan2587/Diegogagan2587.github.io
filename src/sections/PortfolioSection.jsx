cf2712377c3c4547e45c74bf4fa66a5cdc4463a2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-01 14:22:37 -0600

    Pass prop is active to each projectCard component

--- src/sections/PortfolioSection.jsx
+++ src/sections/PortfolioSection.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 import projects from "../data/projects";
 import ProjectCard from "../components/ProjectCard";
-const PortfolioSection = ({sectionRef}) => {
+
+const PortfolioSection = ({sectionRef, isActive}) => {
   return (
     <section id="portfolio" ref={sectionRef} className="portfolio px-6 py-28 flex flex-col gap-24 items-center">
       {/* for each project we will render a component*/}
       {projects.map((project, index) => {
-        return <ProjectCard key={index} project={project} index={index}/>;
+        return <ProjectCard key={index} project={project} index={index} isActive={isActive}/>;
       })}
     </section>
   );
