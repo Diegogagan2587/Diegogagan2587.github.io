05ab52096b7fcc5547b32800b2ec0c9d1c85800e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-30 21:23:36 -0600

    App.jsx --> Implement references and scrollToSection funtion, pass them as props to each component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import IntroductionSection from './sections/IntroductionSection';
 import NavigationBar from './components/NavigationBar';
 import PortfolioSection from './sections/PortfolioSection';
@@ -6,16 +7,34 @@ import ContactForm from './sections/ContactForm';
 
 
 function App() {
+  const introductionRef = useRef(null);
+  const portfolioRef = useRef(null);
+  const aboutRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToSection = (refName) => {
+    const sectionRef = {
+      introductionRef,
+      portfolioRef,
+      aboutRef,
+      contactRef,
+    }[refName];
+
+    if (sectionRef) {
+      sectionRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  };
+
   return (
     <>
       <header className='font-poppins'>
-        <NavigationBar />
+        <NavigationBar scrollToSection={scrollToSection} />
       </header>
       <main className='bg-[#F4F5F7] font-poppins'>
-        <IntroductionSection />
-        <PortfolioSection />
-        <AboutSection />
-        <ContactForm />
+        <IntroductionSection sectionRef={introductionRef}/>
+        <PortfolioSection sectionRef={portfolioRef} />
+        <AboutSection sectionRef={ aboutRef }/>
+        <ContactForm sectionRef={ contactRef }/>
       </main>
     </>
   );
